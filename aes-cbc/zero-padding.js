088af1e760e53fae0cf0e0a9e9c05b184ea7fbbd
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-12-04 21:18:39 +0800

    aes-cbc experiment

--- aes-cbc/zero-padding.js
+++ aes-cbc/zero-padding.js
@@ -1,13 +1,13 @@
 const crypto = require('crypto');
 
 const encrypt = (plain, key, iv = '00000000000000000000000000000000') => {
-    let cipher = crypto.createCipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
+    const cipher = crypto.createCipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
     cipher.setAutoPadding(false);
     return cipher.update(Buffer.from(plain, 'utf8'), 'utf8', 'hex') + cipher.final('hex');
 }
 
 const decrypt = (encrypted, key, iv = '00000000000000000000000000000000') => {
-    let decipher = crypto.createDecipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
+    const decipher = crypto.createDecipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
     decipher.setAutoPadding(false);
     return decipher.update(Buffer.from(encrypted, 'hex'), 'utf8', 'utf8') + decipher.final('utf8');
 }
