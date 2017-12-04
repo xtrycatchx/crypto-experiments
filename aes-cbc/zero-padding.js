0682c51503ca4230279dbc14e6de71d9cdfeada3
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-12-04 21:19:13 +0800

    aes-cbc experiment

--- aes-cbc/zero-padding.js
+++ aes-cbc/zero-padding.js
@@ -14,5 +14,23 @@ const decrypt = (encrypted, key, iv = '00000000000000000000000000000000') => {
 
 const toHexBuffer = string => Buffer.from(string, 'hex')
 
+const zeropad = (input, length = 16) => {
+    let inputInHexString = new Buffer(input, "utf8").toString("hex");
+    console.log(`before padding \t${inputInHexString}`)
+    var bitLength = inputInHexString.length * length;
+
+    if (bitLength < 256) {
+        for (i = bitLength; i < 256; i += length) {
+            inputInHexString += 0;
+        }
+    } else if (bitLength > 256) {
+        while ((inputInHexString.length * length) % 256 != 0) {
+            inputInHexString += 0;
+        }
+    }
+    console.log(`after padding \t${inputInHexString}`)
+    return new Buffer(inputInHexString, "hex").toString("utf8");
+}
+
 
 
