973e160ff2338700b4057b449ba40e3dd2653b39
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-12-04 21:23:16 +0800

    aes-cbc experiment

--- aes-cbc/zero-padding.js
+++ aes-cbc/zero-padding.js
@@ -1,35 +1,32 @@
 const crypto = require('crypto');
 
 const encrypt = (plain, key, iv = '00000000000000000000000000000000') => {
-    const cipher = crypto.createCipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
-    cipher.setAutoPadding(false);
+    const cipher = crypto.createCipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv)).setAutoPadding(false);
     return cipher.update(Buffer.from(plain, 'utf8'), 'utf8', 'hex') + cipher.final('hex');
 }
 
 const decrypt = (encrypted, key, iv = '00000000000000000000000000000000') => {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv));
-    decipher.setAutoPadding(false);
+    const decipher = crypto.createDecipheriv('aes-256-cbc', toHexBuffer(key), toHexBuffer(iv)).setAutoPadding(false);
     return decipher.update(Buffer.from(encrypted, 'hex'), 'utf8', 'utf8') + decipher.final('utf8');
 }
 
 const toHexBuffer = string => Buffer.from(string, 'hex')
 
-const zeropad = (input, length = 16) => {
-    let inputInHexString = new Buffer(input, "utf8").toString("hex");
-    console.log(`before padding \t${inputInHexString}`)
-    var bitLength = inputInHexString.length * length;
-
+const zeropad = (inputData, length = 16) => {
+    inputData = new Buffer(inputData, "utf8").toString("hex");
+    console.log(`before padding \t${inputData}`)
+    const bitLength = inputData.length * length;
     if (bitLength < 256) {
         for (i = bitLength; i < 256; i += length) {
-            inputInHexString += 0;
+            inputData += 0;
         }
     } else if (bitLength > 256) {
-        while ((inputInHexString.length * length) % 256 != 0) {
-            inputInHexString += 0;
+        while ((inputData.length * length) % 256 != 0) {
+            inputData += 0;
         }
     }
-    console.log(`after padding \t${inputInHexString}`)
-    return new Buffer(inputInHexString, "hex").toString("utf8");
+    console.log(`after padding \t${inputData}`)
+    return new Buffer(inputData, "hex").toString("utf8");
 }
 
 const key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
@@ -38,10 +35,10 @@ const iv = "b24bf2f77ac5ec0c5e1f4dc1ae465e75"
 console.log(`aes key \t${key}`)
 console.log(`init vector \t${iv}`)
 
-let before = "Hello World"
-let after = zeropad(before, 16)
+let plain = "Hello World"
+plain = zeropad(plain, 16)
 
-const encrypted = encrypt(after, key, iv)
+const encrypted = encrypt(plain, key, iv)
 console.log(`Encrypted \t${encrypted}`)
 
 const decrypted = decrypt(encrypted, key, iv);
