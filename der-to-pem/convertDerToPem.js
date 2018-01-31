6a15ca931a7704d33887e2e6ef30fe313ab4fd12
Paul Sydney <xtrycatchx@gmail.com>
2018-01-31 17:11:08 +0800

    converting DER formatted certs to PEM

---
+++ der-to-pem/convertDerToPem.js
@@ -0,0 +1,25 @@
+const zlib = require('zlib');
+const fs = require('fs')
+
+const certBinBuffer = fs.readFileSync('mycert.bin')
+const x = Buffer.from(certBinBuffer).toString('hex')
+
+
+const buffer = Buffer.from(x, 'hex');
+zlib.unzip(buffer, (err, buffer) => {
+    if (!err) {
+        const base64OfDer = Buffer.from(buffer).toString('base64')
+        const pemForm = derToPem(base64OfDer)
+        console.log('\n', pemForm)
+    } else {
+        // handle error
+    }
+});
+
+const derToPem = der => {
+    var forge = require("node-forge");
+    var derKey = forge.util.decode64(der);
+    var asnObj = forge.asn1.fromDer(derKey);
+    var asn1Cert = forge.pki.certificateFromAsn1(asnObj);
+    return forge.pki.certificateToPem(asn1Cert);
+};
